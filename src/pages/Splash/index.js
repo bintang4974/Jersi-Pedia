89635d63449b6ef1ea4c52fa993ccde47cb51221
bintang4974 <bintangramadhan4974@gmail.com>
2021-08-10 10:03:03 +0700

    slicing splash screen

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Illustration, Logo } from '../../assets';
 
 export default class Splash extends Component {
-    componentDidMount(){
+    componentDidMount() {
         setTimeout(() => {
-            this.props.navigation.replace('Home')
-        }, 3000)
+            this.props.navigation.replace('Home');
+        }, 3000);
     }
 
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Splash Screen</Text>
+            <View style={styles.page}>
+                <Logo />
+                <View style={styles.illustration}>
+                    <Illustration />
+                </View>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white'
+    },
+    illustration: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0
+    }
+})
