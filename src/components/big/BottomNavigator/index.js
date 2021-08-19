acbd2475ebbb1bae24a52b1302e43dd4a75a2f4c
bintang4974 <bintangramadhan4974@gmail.com>
2021-08-19 14:04:13 +0700

    custom bottom navigator

--- src/components/big/BottomNavigator/index.js
+++ src/components/big/BottomNavigator/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { IconHome, IconHomeActive, IconJersey, IconJerseyActive, IconProfile, IconProfileActive } from '../../../assets';
+import { colors } from '../../../utils';
+
+const Icon = ({ label, focus }) => {
+    switch (label) {
+        case 'Home':
+            return focus ? <IconHomeActive /> : <IconHome />
+        case 'Jersey':
+            return focus ? <IconJerseyActive /> : <IconJersey />
+        case 'Profile':
+            return focus ? <IconProfileActive /> : <IconProfile />
+        default:
+            return <IconHomeActive />
+    }
+}
 
 const BottomNavigator = ({ state, descriptors, navigation }) => {
     const focusedOptions = descriptors[state.routes[state.index].key].options;
@@ -9,7 +24,7 @@ const BottomNavigator = ({ state, descriptors, navigation }) => {
     }
 
     return (
-        <View style={{ flexDirection: 'row' }}>
+        <View style={styles.container}>
             {state.routes.map((route, index) => {
                 const { options } = descriptors[route.key];
                 const label =
@@ -49,9 +64,10 @@ const BottomNavigator = ({ state, descriptors, navigation }) => {
                         testID={options.tabBarTestID}
                         onPress={onPress}
                         onLongPress={onLongPress}
-                        style={{ flex: 1 }}
+                        style={styles.wrapperIcon}
                     >
-                        <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+                        <Icon label={label} focus={isFocused} />
+                        <Text style={styles.text(isFocused)}>
                             {label}
                         </Text>
                     </TouchableOpacity>
@@ -62,3 +78,37 @@ const BottomNavigator = ({ state, descriptors, navigation }) => {
 }
 
 export default BottomNavigator;
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        flexDirection: 'row',
+        backgroundColor: colors.primary,
+        paddingVertical: 8,
+        paddingHorizontal: 34,
+        marginBottom: 30,
+        marginHorizontal: 30,
+        borderRadius: 5,
+        justifyContent: 'space-between',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    },
+    wrapperIcon: {
+        alignItems: 'center'
+    },
+    text: (isFocused) => ({
+        color: isFocused ? colors.white : colors.secondary,
+        fontSize: 11,
+        marginTop: 4
+    })
+});
